dd6d6b17d2b90727369b1ed7f9231f5e102a194f
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-07-04 10:49:28 +0700

    Update Library_Loader.lua

--- Library_Loader.lua
+++ Library_Loader.lua
@@ -41,12 +41,16 @@ local Library = {
 	FontColor = Color3.fromRGB(255, 255, 255);
 	MainColor = Color3.fromRGB(28, 28, 28);
 	BackgroundColor = Color3.fromRGB(20, 20, 20);
-	AccentColor = Color3.fromRGB(0, 85, 255);
+spawn(function()
+	while task.wait() do
+	AccentColor = Color3.fromHSV(tick() % 5/5, 1, 1);
+	end
+end)
 	OutlineColor = Color3.fromRGB(50, 50, 50);
 	RiskColor = Color3.fromRGB(255, 50, 50),
 
 	Black = Color3.new(0, 0, 0);
-	Font = Enum.Font.Code,
+	Font = Enum.Font.Merriweather,
 
 	OpenedFrames = {};
 	DependencyBoxes = {};
